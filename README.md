6a38382a00f435ce8db9dcc38221511049f2b2e4
ashaheed00 <ashaheed00@gmail.com>
2020-10-05 10:07:33 +0530

    adding into branch UC2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TicTacToe
+Version UC2
--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -14,8 +14,27 @@ public class TicTacToeGame {
 		return board;
 	}
 
-	public static void main(String[] args) {
+	private char chooseLettter(char playerLetter) throws Exception {
+
+		if (playerLetter == 'o' || playerLetter == 'x')
+			return playerLetter;
+		else
+			throw new Exception("Choose only o or x");
+	}
+
+	private char computerLetter(char playerLetter) {
+		return playerLetter == 'o' ? 'x' : 'o';
+	}
+
+	public static void main(String[] args) throws Exception {
 		TicTacToeGame ticTacToeGame = new TicTacToeGame();
+		Scanner sc = new Scanner(System.in);
+
+		char playerLetter = sc.next().charAt(0);
+		System.out.println("Player has choosen: " + ticTacToeGame.chooseLettter(playerLetter));
+		System.out.println("Computer's letter : " + ticTacToeGame.computerLetter(playerLetter));
+
+		sc.close();
 
 	}
 
