f78c95ac643177a531eac61e09cdd228f915e58f
ashaheed00 <ashaheed00@gmail.com>
2020-10-07 12:18:23 +0530

    Added: showBoard method to view the board

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TicTacToe
-## Version UC2
-### player chooses letter and determines computer's letter based on that
+## Version UC3
+### Board will be shown
--- src/com/bl/tictactoe/TicTacToeGame.java
+++ src/com/bl/tictactoe/TicTacToeGame.java
@@ -15,18 +15,31 @@ public class TicTacToeGame {
 		return board;
 	}
 
-	// player chooses letter
-	private char chooseLettter(char playerLetter) throws Exception {
-
+	// Giving user a chance to choose preferred letter
+	private char chooseLettter() {
+		System.out.println("Enter your preferred letter (o or x): ");
+		playerLetter = sc.next().charAt(0);
 		if (playerLetter == 'o' || playerLetter == 'x')
 			return playerLetter;
-		else
-			throw new Exception("Choose only o or x");
+		else {
+			System.err.println("Wrong input. Taking 'o' as your choice.");
+			return playerLetter = 'o';
+		}
 	}
 
 	// determines computer's letter based on player's
 	private char computerLetter(char playerLetter) {
 		return playerLetter == 'o' ? 'x' : 'o';
+	}
+
+	// It'll show the board in the console
+	public void showBoard() {
+		System.out.println("***Board***");
+		for (int i = 0; i <= 6; i += 3) {
+			System.out.println(" " + board[i + 1] + " | " + board[i + 2] + " | " + board[i + 3]);
+			if (i < 6)
+				System.out.println("---+---+---");
+		}
 	}
 
 	private static Scanner sc = new Scanner(System.in);
@@ -37,9 +50,9 @@ public class TicTacToeGame {
 		char playerLetter = sc.next().charAt(0);
 		System.out.println("Player has choosen: " + ticTacToeGame.chooseLettter(playerLetter));
 		System.out.println("Computer's letter : " + ticTacToeGame.computerLetter(playerLetter));
+		ticTacToeGame.showBoard();
 
 		sc.close();
 
 	}
-
 }
